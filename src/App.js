bca88fee390ed6e247cea964cab49aed872af914
Greg Doud <gpdoud@gmail.com>
2022-11-19 09:11:21 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Label id="lbl" content="My First Label" />
+      <span> | </span>
+      <Input id="inp" type="text" />
+      <span> | </span>
+      <Button id="btn" text="Click Me!" onClick={ButtonClick} />
     </div>
   );
+}
+function ButtonClick(e) {
+  let inp = document.getElementById("inp").value;
+  console.debug("Inp:", inp);
+}
+function Input(props) {
+  return (
+    <input id={props.id} type={props.type} />
+  )
+}
+function Label(props) {
+  return (
+    <label id={props.id}>{props.content}</label>
+  )
+}
+function Button(props) {
+  return (
+    <button id={props.id} onClick={props.onClick}>{props.text}</button>
+  )
 }
 
 export default App;
